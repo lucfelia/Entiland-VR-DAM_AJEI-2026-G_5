b6517820872df0f751c6700ceddfd696e087b3bc
springcorch <sam.corchero@estudiant.enti.cat>
2026-02-20 17:04:18 +0100

    Initial template

---
+++ Assets/Projects/2026/DAM_AJEI/G_5/Scripts/Template.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace EntilandVR.DosSeis.DAM_AJEI.G_Cinco
+{
+    public class Template : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
