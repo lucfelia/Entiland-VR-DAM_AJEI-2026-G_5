e1b0758654b4c039ec201e8f9f26936393adcf92
PresiTK <sergi.gonzalez.cabrera@estudiant.enti.cat>
2026-02-20 20:02:33 +0100

    movment in X or Y joystick

--- Assets/AutoHand/Scripts/Demo/Gadget Demo/JoystickObjectMover.cs
+++ Assets/AutoHand/Scripts/Demo/Gadget Demo/JoystickObjectMover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,41 @@ namespace Autohand.Demo{
     public class JoystickObjectMover : PhysicsGadgetJoystick{
         public Transform move;
         public float speed = 2;
+        public float movementX;
+        public float movementY;
         
         void Update(){
             var axis = GetValue();
-            var moveAxis = new Vector3(axis.x*Time.deltaTime*speed, 0, axis.y*Time.deltaTime*speed);
+            movementX= axis.x * Time.deltaTime * speed;
+            movementY= axis.y * Time.deltaTime * speed;
+
+            float x = 0.0f;
+            float y = 0.0f;
+            //var moveAxis = new Vector3(axis.x*Time.deltaTime*speed, 0, axis.y*Time.deltaTime*speed);
+            
+            if (Math.Abs(movementX) > Math.Abs(movementY)) {
+                movementY = 0;
+                if (movementX > 0)
+                {
+                    x = 1.0f * Time.deltaTime * speed;
+                }
+                else if (movementX < 0)
+                {
+                    x = -1.0f * Time.deltaTime * speed;
+                }
+            }
+            if (Math.Abs(movementY) > Math.Abs(movementX)) {
+                movementX = 0;
+                if (movementY > 0)
+                {
+                    y = 1.0f * Time.deltaTime * speed;
+                }
+                else if (movementY < 0)
+                {
+                    y = -1.0f * Time.deltaTime * speed;
+                }
+            }
+            var moveAxis = new Vector3(x, 0, y);
             move.transform.localPosition += moveAxis;
         }
     }
