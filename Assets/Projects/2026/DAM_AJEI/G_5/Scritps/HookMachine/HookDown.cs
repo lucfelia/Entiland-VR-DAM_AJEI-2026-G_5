ad295775daf14550016123ef6f2f81f42db1e448
PresiTK <sergi.gonzalez.cabrera@estudiant.enti.cat>
2026-04-14 17:58:50 +0200

    MovementHookUpDown

---
+++ Assets/Projects/2026/DAM_AJEI/G_5/Scritps/HookMachine/HookDown.cs
@@ -0,0 +1,194 @@
+using System.Threading;
+using UnityEngine;
+
+public class HookDown : MonoBehaviour
+{
+    public bool isGrabbing =false;
+    public GameObject startPoint;
+    public GameObject downpoint;
+    public GameObject whenUp;
+    Vector3 positionToDown;
+
+    [SerializeField] private float DownUpTime= 3f;
+    [SerializeField] private float BackTime = 3f;
+    [SerializeField] private float GrabTime= 3f;
+
+    private float timer;
+    
+  public enum HookState
+    {
+        None = 0,   
+        Down=1,
+        Grab =2,
+        Up=3,
+        ReturnBack=4,
+        ReturnLeft=5,
+        UnGrab=6
+    }
+    private HookState hookState;
+
+    void Start()
+    {
+        positionToDown = transform.localPosition;
+        hookState = HookState.Down;
+        Debug.Log("ChangeToDown");
+
+    }
+    private void Update()
+    {
+        switch (hookState)
+        {
+            case HookState.None:
+                return;
+            case HookState.Down:
+                MoveDown();
+                return;
+            case HookState.Grab: 
+                Grab();
+                return;
+            case HookState.Up:
+                MoveUp();
+                return;
+            case HookState.ReturnBack: 
+                ReturnBack();
+                return;
+            case HookState.ReturnLeft: 
+                ReturnLeft();
+                return;
+            case HookState.UnGrab:
+                Ungrab();
+                return;
+            default:
+                return;
+        }
+    }
+    private void MoveDown()
+    {
+        if (timer < DownUpTime)
+        {
+            timer += Time.deltaTime;
+            float t = timer / DownUpTime;
+
+            float newY = Mathf.Lerp(
+                 positionToDown.y,
+                 downpoint.transform.localPosition.y,
+                 t
+             );
+            transform.localPosition = new Vector3(
+                transform.localPosition.x,
+                newY,
+                transform.localPosition.z
+            );
+        }
+        else
+        {
+            hookState = HookState.Grab;
+            timer = 0;
+        }
+    }
+    private void Grab()
+    {
+        if (timer < GrabTime)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            hookState = HookState.Up;
+        }
+    }
+    private void MoveUp()
+    {
+        if (timer < DownUpTime)
+        {
+            timer += Time.deltaTime;
+            float t = timer / DownUpTime;
+
+            float newY = Mathf.Lerp(
+                 downpoint.transform.localPosition.y,
+                 startPoint.transform.localPosition.y,
+                 t
+             );
+
+            transform.localPosition = new Vector3(
+                transform.localPosition.x,
+                newY,
+                transform.localPosition.z
+            );
+    }
+        else
+        {
+            timer = 0;
+            hookState = HookState.ReturnBack;
+        }
+    }
+    private void ReturnBack()
+    {
+        if (timer < DownUpTime)
+        {
+            timer += Time.deltaTime;
+            float t = timer / DownUpTime;
+
+            float newZ = Mathf.Lerp(
+                positionToDown.z,
+                startPoint.transform.localPosition.z,
+                t
+            );
+
+            transform.localPosition = new Vector3(
+                transform.localPosition.x,
+                transform.localPosition.y,
+                newZ
+            );
+        }
+        else
+        {
+            timer = 0;
+            hookState = HookState.ReturnLeft;
+        }
+    }
+    private void ReturnLeft()
+    {
+        if (timer < BackTime)
+        {
+            timer += Time.deltaTime;
+            float t = timer / BackTime;
+            float newX = Mathf.Lerp(
+                positionToDown.x,
+                startPoint.transform.localPosition.x,
+                t
+            );
+
+
+            transform.localPosition = new Vector3(
+                newX,
+                transform.localPosition.y,
+                transform.localPosition.z
+            );       
+        }
+        else
+        {
+            timer = 0;
+            hookState = HookState.UnGrab;
+        }
+    }
+    private void Ungrab()
+    {
+        if(timer< GrabTime)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;
+            hookState = HookState.None;
+        }
+    }
+    public void StartGrabbing()
+    {
+        isGrabbing = true;
+        hookState= HookState.Down;
+        whenUp = gameObject;
+    }
+}
