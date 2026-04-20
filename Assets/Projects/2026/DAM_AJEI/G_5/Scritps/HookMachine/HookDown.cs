1764a81beaf34a235675b05a3693fe1b344fec03
PresiTK <sergi.gonzalez.cabrera@enti.cat>
2026-04-20 22:22:56 +0200

    Tests

--- Assets/Projects/2026/DAM_AJEI/G_5/Scritps/HookMachine/HookDown.cs
+++ Assets/Projects/2026/DAM_AJEI/G_5/Scritps/HookMachine/HookDown.cs
@@ -217,6 +217,8 @@ public class HookDown : MonoBehaviour
     }
     public void StartGrabbing()
     {
-
+        isGrabbing = true;
+        hookState = HookState.Down;
+        positionToDown = transform.localPosition;
     }
 }
